dca3a21f8c318e8060c3ca4300c7c14f768a4808
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-07-01 15:30:54 -0700

    Timer created and fully working

--- Assets/Scripts/HUDScript.cs
+++ Assets/Scripts/HUDScript.cs
@@ -3,13 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class HUDScript : MonoBehaviour
 {
     public Slider slider;
     public GameObject loseScreen;
     public GameObject winScreen;
+    public TMP_Text timer;
+    public float time = 600;
+    
+    void FixedUpdate()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            time = 0;
+        }
 
+        DisplayTime(time);
+
+        if (time == 0)
+        {
+            Lose();
+        }
+    }
+
+    void DisplayTime(float time)
+    {
+        if (time < 0)
+        {
+            time = 0;
+        }
+
+        float min = Mathf.FloorToInt(time / 60);
+        float sec = Mathf.FloorToInt(time % 60);
+        float millisec = time % 1 * 1000;
+
+        timer.text = string.Format("{0:00}:{1:00}", min, sec);
+    }
+    
     public void HP(int health)
     {
         slider.value = health;
