502d3470975715942641dda1e558378468359d72
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-07-12 11:16:48 -0700

    Added comments to enemy attack and changed dugout tag to enemy
    For now, I'll make enemy dugouts before making friendly dugouts.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -30,17 +30,17 @@ public class EnemyAI : MonoBehaviour
     {
         if(Vector2.Distance(transform.position, player.transform.position) < targetRange)
         {
-            Rigidbody2D pRB = player.GetComponent<Rigidbody2D>();
-            Vector2 lookDir = pRB.position - rb.position;
-            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-            rb.rotation = angle;
+            Rigidbody2D pRB = player.GetComponent<Rigidbody2D>(); // Get player rigidbody
+            Vector2 lookDir = pRB.position - rb.position; // get direction from enemy to player
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; // Get the angle
+            rb.rotation = angle; //Rotate the enemy to face the player
 
-            if (Time.time > shootTime)
+            if (Time.time > shootTime) //Attack cooldown
             {
-                GameObject snowball = Instantiate(snowballPrefab, firePoint.position, firePoint.rotation);
-                Rigidbody2D rb = snowball.GetComponent<Rigidbody2D>();
-                rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
-                shootTime = Time.time + fireRate;
+                GameObject snowball = Instantiate(snowballPrefab, firePoint.position, firePoint.rotation); //Instatiate snowball
+                Rigidbody2D rb = snowball.GetComponent<Rigidbody2D>(); // Get snowball rigid body
+                rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); // Give it force
+                shootTime = Time.time + fireRate; //Reset attack cooldown
             }
         }
     }
