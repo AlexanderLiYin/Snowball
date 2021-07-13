67563b041c1898302e45845320fbe978fe567493
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-07-13 12:05:36 -0700

    Created damage field
    As of now, the damage field acts like a giant snowball, next change is to make it placeable from the build menu. After that, make it so the damage is delayed by a second, and then after that make is so the object is destroyed after the delay even if it hasn't damaged anything. Much later I'll have an animation for it.

---
+++ Assets/Scripts/DamageField.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageField : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            EnemyHealth temp;
+            temp = col.gameObject.GetComponent<EnemyHealth>();
+            temp.decHealth(1);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag == "Player")
+        {
+            PlayerHealth temp;
+            temp = col.gameObject.GetComponent<PlayerHealth>();
+            temp.decHealth(1);
+            Destroy(gameObject);
+        }
+        else if (col.gameObject.tag == "Building")
+        {
+            Building temp;
+            temp = col.gameObject.GetComponent<Building>();
+            temp.decHealth(1);
+            Destroy(gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
