e00b1023290893e7f713b172c39ed1dd2d06b22c
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-07-19 15:00:47 -0700

    NPCStateMachine can attack player
    NPCStateMachine correctly attacks the player if they get into range. If the player leaves then the enemy stops attacking.

--- Assets/Scripts/NPCStateMachine.cs
+++ Assets/Scripts/NPCStateMachine.cs
@@ -7,18 +7,21 @@ public class NPCStateMachine : MonoBehaviour
     public Transform firePoint;
     public GameObject snowballPrefab;
     public float bulletForce = 20f;
-    public Rigidbody2D rb;
     public float fireRate = 1;
-
+    public Rigidbody2D rb;
+    public CircleCollider2D cc;
+    float range;
     enum State {idle, attack}
     State state;
     float shootTime = 0;
-    List<GameObject> enemies;
+    GameObject player;
+    // public List<Transform> enemies;
 
     // Start is called before the first frame update
     void Start()
     {
         state = State.idle;
+        range = cc.radius;
     }
 
     // Update is called once per frame
@@ -31,8 +34,47 @@ public class NPCStateMachine : MonoBehaviour
                 break;
 
             case State.attack:
-                print("Attack state");
+                Attack();
                 break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            // Get a referance to the player and switch states
+            player = col.gameObject;
+            state = State.attack;
+        }
+    }
+
+    // Remove player from Check to see if the enemy list is empty, if it is then return to idle. If not, switch target to another enemy.
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            state = State.idle;
+            //enemies.Remove(col.gameObject);
+        }
+    }
+
+    public void Attack()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) < range)
+        {
+            Rigidbody2D pRB = player.GetComponent<Rigidbody2D>(); // Get player rigidbody
+            Vector2 lookDir = pRB.position - rb.position; // get direction from enemy to player
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f; // Get the angle
+            rb.rotation = angle; //Rotate the enemy to face the player
+
+            if (Time.time > shootTime) //Attack cooldown
+            {
+                GameObject snowball = Instantiate(snowballPrefab, firePoint.position, firePoint.rotation); //Instatiate snowball
+                Rigidbody2D rb = snowball.GetComponent<Rigidbody2D>(); // Get snowball rigid body
+                rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse); // Give it force
+                shootTime = Time.time + fireRate; //Reset attack cooldown
+            }
         }
     }
 }
