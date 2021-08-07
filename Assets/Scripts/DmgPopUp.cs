c301499e72171f76931da2c557585306e17914e0
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-08-07 13:39:31 -0700

    Created Damage Pop Up prefab

---
+++ Assets/Scripts/DmgPopUp.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class DmgPopUp : MonoBehaviour
+{
+    TextMeshPro textMesh;
+    public GameObject textPrefab;
+    float moveYSpeed = 20f;
+    float disTime;
+    float disSpeed = 3f;
+    Color textColor;
+
+    public void Create(Vector3 position, int dmg)
+    {
+        GameObject obj = Instantiate(textPrefab, position, Quaternion.identity);
+        DmgPopUp damagePopUp = obj.GetComponent<DmgPopUp>();
+        damagePopUp.Setup(dmg);
+    }
+
+    void Awake()
+    {
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(int damageAmount)
+    {
+        textMesh.SetText(damageAmount.ToString());
+        textColor = textMesh.color;
+        disTime = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+        disTime -= Time.deltaTime;
+        if (disTime < 0)
+        {
+            textColor.a -= disSpeed * Time.deltaTime;
+            textMesh.color = textColor;
+            if(textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
