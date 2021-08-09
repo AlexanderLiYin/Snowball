5086c087f78570ffd78f9231cc52ee7753191370
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-08-09 12:42:33 -0700

    Dialogue is now triggered by button
    Before, the dialogue was automatically triggered on collision.

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    public bool inRange = false;
     public Dialogue dialogue;
     public GameObject UI;
 
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && inRange)
+            TriggerDialogue();
+    }
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        TriggerDialogue();
+        inRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        inRange = false;
     }
 
     public void TriggerDialogue()
