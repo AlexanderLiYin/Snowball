86c82c2142ac8c400f51d1413b7cfaadff330f18
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-08-13 13:51:20 -0700

    Shop menu coin count
    The shop menu displays the amount of coins the player has.

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -2,14 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
     SnowPrincess player;
+    public TMP_Text coins;
 
     void Start()
     {
         player = GameObject.Find("SnowPrincess").GetComponent<SnowPrincess>();
+    }
+
+    void OnEnable()
+    {
+        if(player)
+        coins.text = player.coins.ToString();
     }
 
     void Update()
