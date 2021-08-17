14b5febd85e8b977f8ac0eebdfdebb0a7ff48f78
AlexanderLiYin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-08-17 13:06:11 -0700

    Item description
    The description in the shop menu changes based off which button is pressed.

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -8,10 +8,22 @@ public class Shop : MonoBehaviour
 {
     SnowPrincess player;
     public TMP_Text coins;
+    public TMP_Text descript;
+    public ShopItem[] items;
+    int item;
 
     void Start()
     {
         player = GameObject.Find("SnowPrincess").GetComponent<SnowPrincess>();
+        if(items[0] != null)
+        {
+            descript.SetText(items[0].description);
+            item = 0;
+        }
+        else
+        {
+            descript.SetText("All items have been bought");
+        }
     }
 
     void OnEnable()
@@ -39,6 +51,15 @@ public class Shop : MonoBehaviour
         {
             print("Not enought money");
             return;
+        }
+    }
+
+    public void changeDescrip(int option)
+    {
+        if (items[option] != null)
+        {
+            descript.SetText(items[option].description);
+            item = option;
         }
     }
 
