d27ca908797c5fae06676cd1ac5d21b16e0b1809
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-04 12:21:11 -0700

    Disable Spell indicators
    Spell indicators are now disabled when the spell button or left click is hit.

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Abilities : MonoBehaviour
 {
+    public GameObject targetCircle;
+    public GameObject rangeCircle;
+    public Transform player;
+
+    Vector2 Position;
+    bool active = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,17 @@ public class Abilities : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(!active && Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            targetCircle.SetActive(true);
+            rangeCircle.SetActive(true);
+            active = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha1) || Input.GetButton("Fire1"))
+        {
+            targetCircle.SetActive(false);
+            rangeCircle.SetActive(false);
+            active = false;
+        }
     }
 }
