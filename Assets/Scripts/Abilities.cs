6ad4e87c1bd391293d7157fe2ac6ab2045bb75fb
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-08 12:01:24 -0700

    Added code to ability script

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Abilities : MonoBehaviour
 {
+    // Ability cooldown
+    [Header("Ability 1")]
+    public Image abilityImage1;
+    public float cooldown1 = 5;
+    bool isCooldown = false;
+    public KeyCode ability1;
+
     public GameObject targetCircle;
     public GameObject rangeCircle;
     public Transform player;
@@ -16,12 +24,13 @@ public class Abilities : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        abilityImage1.fillAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Ability1();   
         //Disable/enable ability indicators
         if (!active && Input.GetKeyDown(KeyCode.Alpha1))
         {
@@ -69,5 +78,25 @@ public class Abilities : MonoBehaviour
         rangeCircle.SetActive(false);
         active = false;
         princess.canAttack = true;
+    }
+
+    void Ability1()
+    {
+        if(Input.GetKey(ability1) && isCooldown == false)
+        {
+            isCooldown = true;
+            abilityImage1.fillAmount = 1;
+        }
+
+        if(isCooldown)
+        {
+            abilityImage1.fillAmount -= 1 / cooldown1 * Time.deltaTime;
+
+            if(abilityImage1.fillAmount <= 0)
+            {
+                abilityImage1.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
     }
 }
