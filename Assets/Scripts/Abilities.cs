9692c219d3739668346f9b4f5673eef89cac16bd
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-10 10:38:15 -0700

    Revert "Ability indicator optomisztion"
    This reverts commit 5b02c3df481ba4b0168dc4f8dc9f3e91248c181c.

--- Assets/Scripts/Abilities.cs
+++ Assets/Scripts/Abilities.cs
@@ -32,35 +32,36 @@ public class Abilities : MonoBehaviour
     {
         Ability1();   
         //Disable/enable ability indicators
-        if (!active && Input.GetKeyDown(KeyCode.Alpha1) && isCooldown == false)
+        if (!active && Input.GetKeyDown(KeyCode.Alpha1))
         {
             Enable();
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 lookDir = (mousePos - (Vector2)player.position).normalized;
-
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                if (hit.collider.gameObject != this.gameObject)
-                {
-                    posUp = new Vector2(hit.point.x, 10f);
-                    position = hit.point;
-                }
-            }
-
-            var hitPosDir = (hit.point - transform.position).normalized;
-            float distance = Vector2.Distance(hit.point, transform.position);
-            distance = Mathf.Min(distance, maxDistance);
-
-            var newHitPos = (Vector2)transform.position + (lookDir * distance);
-            targetCircle.transform.position = (newHitPos);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             Disable();
         }
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 lookDir = (mousePos - (Vector2)player.position).normalized;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            if (hit.collider.gameObject != this.gameObject)
+            {
+                posUp = new Vector2(hit.point.x, 10f);
+                position = hit.point;
+            }
+        }
+
+        var hitPosDir = (hit.point - transform.position).normalized;
+        float distance = Vector2.Distance(hit.point, transform.position);
+        distance = Mathf.Min(distance, maxDistance);
+
+        var newHitPos = (Vector2)transform.position + (lookDir * distance);
+        targetCircle.transform.position = (newHitPos);
     }
 
     public void Enable()
@@ -85,7 +86,6 @@ public class Abilities : MonoBehaviour
         {
             isCooldown = true;
             abilityImage1.fillAmount = 0;
-            //Disable();
         }
 
         if(isCooldown)
