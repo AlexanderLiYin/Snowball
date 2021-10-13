14ed2dcd01f19e8588eeff214323e1e69b1bfa6d
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-13 09:18:11 -0700

    Imported Ultimate Inventory System

---
+++ Assets/Opsive/UltimateInventorySystem/Scripts/UI/CompoundElements/Text.cs
@@ -0,0 +1,10 @@
+﻿/// ---------------------------------------------
+/// Ultimate Inventory System
+/// Copyright (c) Opsive. All Rights Reserved.
+/// https://www.opsive.com
+/// ---------------------------------------------
+
+namespace Opsive.UltimateInventorySystem.UI.CompoundElements
+{
+    //TODO Deprecated, the Text class is now in the shared folder.
+}
