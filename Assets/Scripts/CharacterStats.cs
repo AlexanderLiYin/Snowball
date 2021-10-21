18ea990acc288250a15b4ce446bff553492c3f11
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-21 12:06:25 -0700

    UI now displays player max health

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -12,15 +12,21 @@ public class CharacterStats : MonoBehaviour
     [Tooltip("The defense text.")]
     [SerializeField] protected TMP_Text m_DefenseValueText;
 
+    SnowPrincess player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("SnowPrincess").GetComponent<SnowPrincess>();
+        Draw();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Draw()
     {
-        
+        //if (m_CharacterStats == null) { Initialize(); }
+
+        //m_MaxHpValueText.text = m_CharacterStats.MaxHp.ToString();
+        m_MaxHpValueText.text = player.maxHealth.ToString();
+        //m_AttackValueText.text = m_CharacterStats.Attack.ToString();
+        //m_DefenseValueText.text = m_CharacterStats.Defense.ToString();
     }
 }
