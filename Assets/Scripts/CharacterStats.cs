6f320b152517d2d05a9d8ffa25c36f17d0e8348a
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-10-21 11:59:14 -0700

    Created new stat display script

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CharacterStats : MonoBehaviour
+{
+    [Tooltip("The max Hp text.")]
+    [SerializeField] protected TMP_Text m_MaxHpValueText;
+    [Tooltip("The attack text.")]
+    [SerializeField] protected TMP_Text m_AttackValueText;
+    [Tooltip("The defense text.")]
+    [SerializeField] protected TMP_Text m_DefenseValueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
