e1c168db73c12957b47758967adaa3d1c5e8fae2
Alexander Li Yin <43706420+AlexanderLiYin@users.noreply.github.com>
2021-12-06 15:27:48 -0800

    Created Seperate Script for Snow Energy
    Now renamed mana. Commiting before deleting code in Snow Princess.

--- Assets/Scripts/Mana.cs
+++ Assets/Scripts/Mana.cs
@@ -4,15 +4,87 @@ using UnityEngine;
 
 public class Mana : MonoBehaviour
 {
+    //Energy
+    HUDScript HUD;
+    public int initEnergy = 50;
+    public int maxEnergy = 120;
+    public float energyPerSec = 1;
+    float energy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        try {HUD = GameObject.Find("HUD").GetComponent<HUDScript>(); }
+        catch { print("Error, HUD not found"); }
+        // Display Energy
+        energy = initEnergy;
+        HUD.DisplayEnergy(energy, maxEnergy);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Energy Generation
+        if (energy < maxEnergy)
+        {
+            energy += Time.deltaTime * energyPerSec;
+        }
+        else
+        {
+            energy = maxEnergy;
+        }
+        HUD.DisplayEnergy(energy, maxEnergy);
+    }
+
+    public void incEnergy(int inc)
+    {
+        if ((inc + energy) > maxEnergy)
+            energy = maxEnergy;
+        else
+        {
+            energy = energy + inc;
+        }
+    }
+
+    public bool decEnergy(int cost)
+    {
+        if (cost > energy)
+            return false;
+        else
+        {
+            energy = energy - cost;
+            return true;
+        }
+    }
+
+    public void incMaxEnergy(int energy)
+    {
+        maxEnergy += energy;
+        HUD.DisplayEnergy(energy, maxEnergy);
+    }
+
+    public bool decMaxEnergy(int energy)
+    {
+        if ((maxEnergy - energy) > 0)
+        {
+            maxEnergy -= energy;
+            return true;
+        }
+        else return false;
+    }
+
+    public void incEnergyRecharge(float rate)
+    {
+        energyPerSec += rate;
+    }
+
+    public bool decEnergyRecharge(float rate)
+    {
+        if ((energyPerSec - rate) > 0)
+        {
+            energyPerSec -= rate;
+            return true;
+        }
+        else return false;
     }
 }
